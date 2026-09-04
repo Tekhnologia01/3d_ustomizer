1b5cf4677c971c4fc76413964897a573459050bc
RushikeshTekhnologia <rushikesh.patil@gmail.com>
2026-09-04 21:30:55 +0530

    new backend changes

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,17 +1,34 @@
-from django.urls import path, include
+# from django.urls import path, include
+# from django.conf import settings
+# from django.conf.urls.static import static
+# from customizer.views import create_tripo_model
+
+
+# urlpatterns = [
+#     path('', include('customizer.urls')),
+# ]
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+# urlpatterns += [
+#     path("api/tripo/create/", create_tripo_model),
+# ]
+
+
+
+
+
+
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 from customizer.views import create_tripo_model
 
 
 urlpatterns = [
     path('', include('customizer.urls')),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
-urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     path("api/tripo/create/", create_tripo_model),
 ]
